bf994439526b3cbbecbe43bcfbf89605b2930ae0
Ruthuparna <ruthu199425@gmail.com>
2022-10-27 22:13:38 +0530

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reg';
+  topics = ['Angular', 'React', 'Java', 'Python'];
+
+   topicHasError= true;
+
+  userModel = new User('', 'rob@gmail.com', 1234567890, 'default', 'morning', true);
+
+  validateTopic(value:string){
+   if(value === 'default')
+   {
+    this.topicHasError= true;
+   }
+   else
+   {
+    this.topicHasError= false;
+   }
+  }
 }
